00ace27369a9cd5b1fb1a11516c37732b0a6293e
ClementCollignon <clement.collignon.0@gmail.com>
2023-10-20 11:10:38 -0400

    code cleaned

--- example.py
+++ example.py
@@ -8,4 +8,4 @@ wall_thickness = 2
 maximum_randomness = 15
 
 vase = Vase(height, radius, number_points_z, number_points_theta, wall_thickness, maximum_randomness)
-vase.generate_random_vase(r'')
+vase.generate_random_vase('')
